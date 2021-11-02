41ac11e78be90c26174e0c3ab136b32a426d9dce
BenSmile <benjkafirongo@gmail.com>
2021-11-02 19:32:02 +0100

    Header compnoent style finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
           <Login />
+        </Route>
+        <Route exact path="/home">
+          <Header />
+          <Home />
         </Route>
       </Switch>
     </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
-import React from "react";
 import styled from "styled-components";
 
-const Login = () => {
+const Login = (props) => {
   return (
     <div>
       <Container>
